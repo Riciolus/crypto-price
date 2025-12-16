99036654d9ae6d297507fbb59d0dc1592d3e9be8
Marcell Pramana <marcellpramana@gmail.com>
2025-12-16 21:17:08 +0700

    feat: small updates UI on header

--- app/page.tsx
+++ app/page.tsx
@@ -44,34 +44,38 @@ export default function Home() {
   if (error) return <p>Error: {error}</p>;
 
   return (
-    <div className="p-3 flex flex-col space-y-5 ">
-      <Header />
+    <div className="flex justify-center ">
+      <div className="p-3 flex flex-col space-y-5 w-full max-w-400 px-10 lg:px-6">
+        <Header />
 
-      <div className="space-y-2 md:space-y-1">
-        <p className="text-sm font-semibold">Bitcoin Price Chart (BTC)</p>
+        <div className="space-y-2 md:space-y-1">
+          <p className="text-sm font-semibold text-gray-400">
+            Bitcoin Price Chart (BTC)
+          </p>
 
-        <div className="md:flex md:justify-between md:flex-row-reverse space-y-1 md:space-y-0">
-          <div className="py-1   flex space-x-2 rounded-lg bg-gray-700/30 h-fit text-gray-300/90 w-fit font-semibold">
-            <Button>24H</Button>
-            <Button>7D</Button>
-            <Button>1M</Button>
-            <Button>3M</Button>
-          </div>
-
-          <div className="flex md:flex-col space-y-1 space-x-1">
-            <div className="py-1 flex space-x-2 rounded-lg bg-gray-700/30 text-gray-300/90 w-fit h-fit font-semibold">
-              <Button>Price</Button>
-              <Button>Market Cap</Button>
-              <Button>TradingView</Button>
+          <div className="md:flex md:justify-between md:flex-row-reverse space-y-1 md:space-y-0">
+            <div className="py-1   flex space-x-2 rounded-lg bg-gray-700/30 h-fit text-gray-300/90 w-fit font-semibold">
+              <Button>24H</Button>
+              <Button>7D</Button>
+              <Button>1M</Button>
+              <Button>3M</Button>
             </div>
-            <div className="py-1 flex space-x-2 rounded-lg bg-gray-700/30 text-gray-300/90 w-fit h-fit font-semibold">
-              <Button>a</Button>
-              <Button>a</Button>
+
+            <div className="flex md:flex-col space-y-1 space-x-1">
+              <div className="py-1 flex space-x-2 rounded-lg bg-gray-700/30 text-gray-300/90 w-fit h-fit font-semibold">
+                <Button>Price</Button>
+                <Button>Market Cap</Button>
+                <Button>TradingView</Button>
+              </div>
+              <div className="py-1 flex space-x-2 rounded-lg bg-gray-700/30 text-gray-300/90 w-fit h-fit font-semibold">
+                <Button>a</Button>
+                <Button>a</Button>
+              </div>
             </div>
           </div>
+          <div>{!loading && data && <PriceChart candles={data} />}</div>
         </div>
       </div>
-      <div>{!loading && data && <PriceChart candles={data} />}</div>
     </div>
   );
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -2,13 +2,17 @@ import Image from "next/image";
 
 export default function Header() {
   return (
-    <div className="">
-      <div className="flex items-center space-x-2">
+    <div className="pt-5 space-y-2">
+      <div className="flex items-center space-x-2 ">
         <div className="relative w-6 h-6">
           <Image alt="" src={"/bitcoin.webp"} fill />
         </div>
-        <h1 className="text-xl font-semibold">Bitcoin</h1>
+        <h1 className="text-xl font-semibold text-neutral-100">Bitcoin</h1>
         <h3 className="text-lg font-semibold text-zinc-500 font-mono">#BTC</h3>
+      </div>
+
+      <div>
+        <h2 className="text-4xl font-bold">$92,012.53</h2>
       </div>
     </div>
   );
