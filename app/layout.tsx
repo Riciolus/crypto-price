43132821f36683a91a5cecb4159c77ed567ef299
Marcell Pramana <marcellpramana@gmail.com>
2025-12-23 23:16:48 +0700

    chore(ui): update favicon and small UI tweaks

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Link from "next/link";
+import Image from "next/image";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +29,8 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <nav>
           <Link href="/">
-            <div className="w-full border-b border-neutral-700/50 px-5 py-3 bg-neutral-950">
+            <div className="w-full border-b flex items-center gap-3 border-neutral-700/50 px-5 py-3 bg-neutral-950">
+              <Image alt="" src="/logo.png" width={32} height={32} />
               <h1 className="text-xl font-bold font-mono">JarJar Coin</h1>
             </div>
           </Link>
